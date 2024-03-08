02bd60d135e5b64792ced31c6847d16b58ee4f5c
hyuga_waki <waki.hyuga@synergy-effect.jp>
2024-03-08 10:30:36 +0900

    done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
+import { NavBar } from "@/components/NavBar";
+import { AuthProvider } from "@/context/auth";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <div>
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+    </AuthProvider>
+  );
 }
