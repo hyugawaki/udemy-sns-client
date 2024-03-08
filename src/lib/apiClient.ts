17864c789229f190278eb0eaf7db421e8566cefe
hyuga_waki <waki.hyuga@synergy-effect.jp>
2024-03-08 11:07:35 +0900

    fix

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8000/api",
-    headers: {
-        "Content-Type": "application/json"
-    },
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
